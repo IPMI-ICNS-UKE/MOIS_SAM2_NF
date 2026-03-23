d9e00e41bcd726b7da99c76e2d354bdd7eb02730
SophieSchou <sophie.schouten@studium.uni-hamburg.de>
2026-03-23 16:52:40 +0100

    Heatmaps labeling update

--- segmentation_analysis/main.py
+++ segmentation_analysis/main.py
@@ -1,5 +1,11 @@
-from src.calculate_dice_scores import calculate_dice_scores_by_pat_files, save_dice_scores_by_pat_to_csv
-from src.calculate_mean_dice_scores import calculate_mean_dice_scores_by_pairs, save_mean_dice_scores_to_csv
+from src.calculate_dice_scores import (
+    calculate_dice_scores_by_pat_files,
+    save_dice_scores_by_pat_to_csv,
+)
+from src.calculate_mean_dice_scores import (
+    calculate_mean_dice_scores_by_pairs,
+    save_mean_dice_scores_to_csv,
+)
 from src.compare_mask_shapes import compare_mask_shapes
 from src.create_overlay_masks import save_overlay_masks
 from src.load_masks import load_and_unify_masks
@@ -44,19 +50,17 @@ if __name__ == "__main__":
     )
 
     print("6/8 Save mean dice scores by rater pairs to CSV")
-    mean_dice_csv_path = save_mean_dice_scores_to_csv(
-        mean_dice_scores=mean_dice_scores
-    )
+    mean_dice_csv_path = save_mean_dice_scores_to_csv(mean_dice_scores=mean_dice_scores)
     print(f"   Mean Dice Scores CSV saved: {mean_dice_csv_path}")
 
     print("7/8  Create heatmaps")
-    mean_dice_heatmap_path = create_mean_dice_heatmap(
-        mean_dice_scores=mean_dice_scores
-    )
+    # mean_dice_heatmap_path = create_mean_dice_heatmap(
+    #     mean_dice_scores=mean_dice_scores
+    # )
     dice_heatmap_paths_by_pat = create_dice_heatmaps_by_pat(
         dice_scores_by_file=dice_scores_by_file
     )
-    print(f"   Mean heatmap saved: {mean_dice_heatmap_path}")
+    # print(f"   Mean heatmap saved: {mean_dice_heatmap_path}")
     print(
         f"   Heatmaps by patient saved: {len(dice_heatmap_paths_by_pat)} in {DICE_HEATMAPS_BY_PAT_DIR}"
     )
